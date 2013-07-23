398d41b9cc4bdf035e882f32396d9a593f63b04d
admin <nemiroff4@yahoo.com>
2013-07-22 21:22:01 -0700

    7/22/13, Worked on intersection with the planets and BOOM!!!

--- src/SpaceAge.java
+++ src/SpaceAge.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Ellipse2D;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
@@ -16,7 +17,8 @@ import javax.swing.Timer;
 import javax.swing.WindowConstants;
 
 /*
- * 5/25/13
+ * 7/22/13
+ * worked on intersection with the planets and BOOM!!!
  */
 public class SpaceAge extends JComponent implements ActionListener, KeyListener
 {
@@ -29,10 +31,13 @@ public class SpaceAge extends JComponent implements ActionListener, KeyListener
 	Image planet4;
 	Image planet5;
 	Image spaceShip;
+	Image boom;
 	int width = Toolkit.getDefaultToolkit().getScreenSize().width - 20;
 	int height = width/2;
 	int spaceX = width/2;
 	int spaceY = height/2;
+	int boomX;
+	int boomY;
 	int numOfPlanets = 20;
 	boolean up = false;
 	boolean down = false;
@@ -51,6 +56,7 @@ public class SpaceAge extends JComponent implements ActionListener, KeyListener
 	ArrayList<Integer> randomX = new ArrayList<Integer>();
 	ArrayList<Integer> randomY = new ArrayList<Integer>();
 	ArrayList<Integer> randomP = new ArrayList<Integer>();
+	ArrayList<Ellipse2D> planets = new ArrayList<Ellipse2D>();
 	
 	public SpaceAge() throws IOException
 	{
@@ -63,6 +69,7 @@ public class SpaceAge extends JComponent implements ActionListener, KeyListener
 		planet4 = ImageIO.read(getClass().getResource("planet6.jpg")).getScaledInstance((int) (height/4.0), (int) (height/4.0), 0);
 		planet5 = ImageIO.read(getClass().getResource("planet7.jpg")).getScaledInstance((int) (height/4.0), (int) (height/4.0), 0);
 		spaceShip = ImageIO.read(getClass().getResource("spaceShip.jpg")).getScaledInstance((int) (height/4.0), -1, 0);
+		boom = ImageIO.read(getClass().getResource("boom.png")).getScaledInstance((int) (spaceShip.getWidth(null) * 1.2), -1, 0);
 		spaceX -= spaceShip.getWidth(null)/2;
 		spaceY -= spaceShip.getHeight(null)/2;
 	}
@@ -178,6 +185,8 @@ public class SpaceAge extends JComponent implements ActionListener, KeyListener
 				theXFactor -= speed;
 			}
 		}
+		boomX = (int) (spaceX - spaceShip.getWidth(null) * 0.15);
+		boomY = spaceY - boom.getHeight(null)/2 + spaceShip.getHeight(null)/2;
 		//background
 		for (int i = 0; i < 2; i++) 
 		{
@@ -213,36 +222,41 @@ public class SpaceAge extends JComponent implements ActionListener, KeyListener
 					}
 				}
 			}
+			for (int i = 0; i < numOfPlanets; i++) 
+			{
+				planets.add(new Ellipse2D.Double(randomX.get(i) + width/2 + spaceShip.getWidth(null)/2, randomY.get(i), planet0.getWidth(null), planet0.getHeight(null)));
+			}
 		}
 		//paint the planets
 		for (int i = 0; i < numOfPlanets; i++) 
 		{
-			int X = randomX.get(i);
+			int X = randomX.get(i) + width/2 + spaceShip.getWidth(null)/2 - theXFactor;
 			int Y = randomY.get(i);
 	     	if (randomP.get(i) == 0) 
 			{
-				g.drawImage(planet0, X + width/2 + spaceShip.getWidth(null)/2 - theXFactor, Y, null);
+				g.drawImage(planet0, X, Y, null);
 			}
 			else if (randomP.get(i) == 1) 
 			{
-				g.drawImage(planet1, X + width/2 + spaceShip.getWidth(null)/2 - theXFactor, Y, null);
+				g.drawImage(planet1, X, Y, null);
 			}
 			else if (randomP.get(i) == 2) 
 			{
-				g.drawImage(planet2, X + width/2 + spaceShip.getWidth(null)/2 - theXFactor, Y, null);
+				g.drawImage(planet2, X, Y, null);
 			}
 			else if (randomP.get(i) == 3) 
 			{
-				g.drawImage(planet3, X + width/2 + spaceShip.getWidth(null)/2 - theXFactor, Y, null);
+				g.drawImage(planet3, X, Y, null);
 			}
 			else if (randomP.get(i) == 4) 
 			{
-				g.drawImage(planet4, X + width/2 + spaceShip.getWidth(null)/2 - theXFactor, Y, null);
+				g.drawImage(planet4, X, Y, null);
 			}
 			else if (randomP.get(i) == 5) 
 			{
-				g.drawImage(planet5, X + width/2 + spaceShip.getWidth(null)/2 - theXFactor, Y, null);
+				g.drawImage(planet5, X, Y, null);
 			}
+	     	//g.fillOval((int) planets.get(i).getX() - theXFactor, (int) planets.get(i).getY(), (int) planets.get(i).getWidth(), (int) planets.get(i).getHeight());
 		}
 		g.drawImage(livablePlanet, width * length - livablePlanet.getWidth(null) - theXFactor, height/2 - livablePlanet.getHeight(null)/2, null);
 		//laser
@@ -270,6 +284,15 @@ public class SpaceAge extends JComponent implements ActionListener, KeyListener
 		}
 		//space ship
 		g.drawImage(spaceShip, spaceX - theXFactor, spaceY, null);
+		for (int i = 0; i < numOfPlanets; i++) 
+		{
+			//In testing
+			//if (spaceX + spaceShip.getWidth(null) > randomX.get(i) + width/2 + spaceShip.getWidth(null)/2 && spaceX < randomX.get(i) + width/2 + spaceShip.getWidth(null)/2 + planet0.getWidth(null) && spaceY + spaceShip.getHeight(null) > randomY.get(i) && spaceY < randomY.get(i) + planet0.getHeight(null)) {
+			if (planets.get(i).intersects(spaceX, spaceY, spaceShip.getWidth(null), spaceShip.getHeight(null))) {
+				g.drawImage(boom, boomX - theXFactor, boomY, null);
+			}
+		}
+		//g.drawImage(boom, boomX - theXFactor, boomY, null);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e)
